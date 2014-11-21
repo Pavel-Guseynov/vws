b90034eb435814e8ff1a25622a05bb87f5041649
Pavel Guseynov <pavel.guseynov@gmail.com>
2014-11-21 18:46:12 +0300

    add tagret without reading file

--- lib/vws.rb
+++ lib/vws.rb
@@ -15,121 +15,143 @@ module Vws
    SUMMARY_URL = BASE_URL + "/summary"
 
   class Api
-  
+
     def initialize(accesskey=nil, secretkey=nil)
       @accesskey = accesskey || ENV['VWS_ACCESSKEY']
       @secretkey = secretkey || ENV['VWS_SECRETKEY']
     end
 
     def build_signature(request_path, body_hash, http_verb, timestamp)
-      # request_path signifies the suburi you call after you subtract the 
-      # BASE_URL; that is, if you call https://vws/vuforia.com/targets, 
-      # the request_path is '/targets' 
-      
+      # request_path signifies the suburi you call after you subtract the
+      # BASE_URL; that is, if you call https://vws/vuforia.com/targets,
+      # the request_path is '/targets'
+
       contentType = ""
-      hexDigest = "d41d8cd98f00b204e9800998ecf8427e" # Hex digest of an empty 
-                                                     # string. We use it to 
+      hexDigest = "d41d8cd98f00b204e9800998ecf8427e" # Hex digest of an empty
+                                                     # string. We use it to
                                                      # signify empty body
 
-      if http_verb == "GET" || http_verb == "DELETE" 
+      if http_verb == "GET" || http_verb == "DELETE"
         # Do nothing since we have already set contentType and hexDigest
-      elsif http_verb == "POST" || http_verb == "PUT" 
+      elsif http_verb == "POST" || http_verb == "PUT"
         contentType = "application/json";
         # the request should have a request body, so create an md5 hash of that
         # json body data
         hexDigest = Digest::MD5.hexdigest(body_hash.to_json)
-      else 
+      else
         puts "Invalid request method";
         return nil
       end
-      
+
       toDigest  = http_verb   + "\n" +
                   hexDigest   + "\n" +
                   contentType + "\n" +
                   timestamp   + "\n" +
                   request_path
 
-      return Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new, 
-                              @secretkey, toDigest))  
+      return Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new,
+                              @secretkey, toDigest))
     end
 
-    # Calls the api end point for the list of targets associated with 
+    # Calls the api end point for the list of targets associated with
     # server access key and cloud database
     def list_targets
-      #Date is the current date per RFC 2616, section 3.3.1, 
+      #Date is the current date per RFC 2616, section 3.3.1,
       #rfc1123-date format, e.g.: Sun, 22 Apr #2012 08:49:37 GMT.
-      date_timestamp = Time.now.httpdate #ruby provides this date format 
+      date_timestamp = Time.now.httpdate #ruby provides this date format
                                          #with httpdate method
       signature = self.build_signature('/targets', nil, 'GET', date_timestamp)
       authorization_header = "VWS " + @accesskey + ":" +  signature
       begin
-        RestClient.get(TARGETS_URL, :'Date' => date_timestamp, 
+        RestClient.get(TARGETS_URL, :'Date' => date_timestamp,
                                     :'Authorization' => authorization_header)
       rescue => e
         e.response
       end
     end
 
-    def add_target(target_name, file_path, width, active_flag)
-      raise "file path is required"   if file_path.nil?
+    def add_target_with_data(target_name, data, width, active_flag)
+      raise "data is required"   if data.nil?
       raise "target name is required" if target_name.nil?
-      date_timestamp = Time.now.httpdate 
+      date_timestamp = Time.now.httpdate
       #for file uploads, read file contents data and Base 64 encode it:
-      contents_encoded = Base64.encode64(File.open(file_path, 'rb').read)
-      body_hash = { :name => target_name, 
+      body_hash = { :name => target_name,
                     :width => width, #width of the target in scene units
-                    :image => contents_encoded, 
+                    :image => data,
                     :active_flag => active_flag }
       signature = self.build_signature('/targets', body_hash, 'POST', date_timestamp)
       authorization_header = "VWS " + @accesskey + ":" +  signature
       begin
-        RestClient.post(TARGETS_URL, body_hash.to_json, 
-                                    :'Date' => date_timestamp, 
-                                    :'Authorization' => authorization_header, 
-                                    :content_type => 'application/json', 
+        RestClient.post(TARGETS_URL, body_hash.to_json,
+                                    :'Date' => date_timestamp,
+                                    :'Authorization' => authorization_header,
+                                    :content_type => 'application/json',
                                     :accept => :json)
       rescue => e
         e.response
       end
     end
-    
+
+    def add_target_with_file(target_name, file_path, width, active_flag)
+      raise "file path is required"   if file_path.nil?
+      raise "target name is required" if target_name.nil?
+      date_timestamp = Time.now.httpdate
+      #for file uploads, read file contents data and Base 64 encode it:
+      contents_encoded = Base64.encode64(File.open(file_path, 'rb').read)
+      body_hash = { :name => target_name,
+                    :width => width, #width of the target in scene units
+                    :image => contents_encoded,
+                    :active_flag => active_flag }
+      signature = self.build_signature('/targets', body_hash, 'POST', date_timestamp)
+      authorization_header = "VWS " + @accesskey + ":" +  signature
+      begin
+        RestClient.post(TARGETS_URL, body_hash.to_json,
+                                    :'Date' => date_timestamp,
+                                    :'Authorization' => authorization_header,
+                                    :content_type => 'application/json',
+                                    :accept => :json)
+      rescue => e
+        e.response
+      end
+    end
+
     def update_target(target_id, target_name=nil, file_path=nil, width=nil, active_flag=nil)
       date_timestamp = Time.now.httpdate
       target_id_url = TARGETS_URL + '/' + target_id
-      target_id_suburl = '/targets' + '/' + target_id  
+      target_id_suburl = '/targets' + '/' + target_id
       #for file uploads, read file contents data and Base 64 encode it:
       contents_encoded = Base64.encode64(File.open(file_path, 'rb').read)
-      body_hash = { :name => target_name, 
+      body_hash = { :name => target_name,
                     :width => width, #Width of the target in scene unit
-                    :image => contents_encoded, 
+                    :image => contents_encoded,
                     :active_flag => active_flag }
       signature = self.build_signature(target_id_suburl, body_hash, 'PUT', date_timestamp)
       authorization_header = "VWS " + @accesskey + ":" +  signature
       begin
-        RestClient.put(target_id_url, body_hash.to_json, 
-                                    :'Date' => date_timestamp, 
-                                    :'Authorization' => authorization_header, 
-                                    :content_type => 'application/json', 
+        RestClient.put(target_id_url, body_hash.to_json,
+                                    :'Date' => date_timestamp,
+                                    :'Authorization' => authorization_header,
+                                    :content_type => 'application/json',
                                     :accept => :json)
       rescue => e
         e.response
       end
     end
-    
+
     # Database Summary Report
     # https://developer.vuforia.com/resources/dev-guide/database-summary-report
     def summary
       date_timestamp = Time.now.httpdate
-      signature = self.build_signature('/summary', nil, 'GET', date_timestamp) 
+      signature = self.build_signature('/summary', nil, 'GET', date_timestamp)
       authorization_header = "VWS " + @accesskey + ":" + signature
       begin
-        RestClient.get(SUMMARY_URL, :'Date' => date_timestamp, 
+        RestClient.get(SUMMARY_URL, :'Date' => date_timestamp,
                                     :'Authorization' => authorization_header)
       rescue => e
         e.response
       end
     end
-    
+
     # Retrieve a target from the cloud database
     # https://developer.vuforia.com/resources/dev-guide/retrieving-target-cloud-database
     def retrieve_target(target_id)
@@ -159,7 +181,7 @@ module Vws
                                       :'Authorization' => authorization_header)
       rescue => e
         e.response
-      end      
+      end
     end
 
     def set_active_flag(target_id, active_flag)
@@ -169,12 +191,12 @@ module Vws
       body_hash = {:active_flag => active_flag}
       signature = self.build_signature(target_id_suburl, body_hash, 'PUT', date_timestamp)
       authorization_header = "VWS " + @accesskey + ":" + signature
-     
+
       begin
-        RestClient.put(target_id_url, body_hash.to_json, 
-                                      :'Date' => date_timestamp, 
-                                      :'Authorization' => authorization_header, 
-                                      :content_type => 'application/json', 
+        RestClient.put(target_id_url, body_hash.to_json,
+                                      :'Date' => date_timestamp,
+                                      :'Authorization' => authorization_header,
+                                      :content_type => 'application/json',
                                       :accept => :json)
       rescue => e
           e.response
@@ -191,14 +213,14 @@ module Vws
       # retrieve_target's results since they're in string format
       target_result_code = target_data["result_code"]
       if target_result_code != "AuthenticationFailure"
-        if target_result_code != "UnknownTarget"   
+        if target_result_code != "UnknownTarget"
           target_active_flag = target_data["target_record"]["active_flag"]
           target_status = target_data["status"]
           if target_result_code == "Success"
             if target_active_flag == true && target_status == "success"
               return {:result_code => "TargetActive"}.to_json
             elsif target_active_flag == false && target_status == "success"
-              # if we reached this point, the target is fine, inactive and 
+              # if we reached this point, the target is fine, inactive and
               # ready to be deleted
               date_timestamp = Time.now.httpdate
               target_id_url = TARGETS_URL + '/' + target_id
@@ -206,7 +228,7 @@ module Vws
               signature = self.build_signature(target_id_suburl, nil, 'DELETE', date_timestamp)
               authorization_header = "VWS " + @accesskey + ":" + signature
                 begin
-                  RestClient.delete(target_id_url, 
+                  RestClient.delete(target_id_url,
                                              :'Date' => date_timestamp,
                                              :'Authorization' => authorization_header)
                 rescue => e
@@ -215,7 +237,7 @@ module Vws
             else
               return {:result_code => "#{target_status}"}.to_json
             end
-          end  
+          end
         else
           return {:result_code => "UnknownTarget"}.to_json
         end
@@ -223,6 +245,6 @@ module Vws
         return {:result_code => "AuthenticationFailure"}.to_json
       end
     end
-  
+
   end
 end
